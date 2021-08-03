189de41786ce64d28b3cc5c15e13e2045b086dee
Frankie Murillo <frankidatank@gmail.com>
2021-08-03 15:41:08 -0700

    testing routes

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+# connect to postgres server
+@app.route("/destinations")
+def get_destinations():
+    return "Test"
