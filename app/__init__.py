558aa7222bf3bbe2537e3005be4ca8407a9511e6
Frankie Murillo <frankidatank@gmail.com>
2021-08-05 21:28:15 -0700

    update routes

--- app/__init__.py
+++ app/__init__.py
@@ -83,11 +83,15 @@ google = oauth.register(
 
 
 @app.route('/')
-def hello_world():
+def landing_page():
     email = dict(session).get('email', None)
     name = dict(session).get('name', None)
-    picture = dict(session).get('picture', None)
-    return f'Hello, you are logged in as {email}! And your name is {name}'
+    return render_template("template name here")
+
+
+@app.route('/planner')
+def planner_page():
+    return render_template("template name here")
 
 
 @app.route('/login')
@@ -154,139 +158,3 @@ def deleteUserInfo(userId):
     deleteThis = Destination.query.filter_by(id=userId).first() 
     db.session.delete(deleteThis)
 
-
-
-
-
-
-
-
-
-
-
-
-
-# import os
-# from flask import Flask, render_template, send_from_directory, request
-# from werkzeug.security import generate_password_hash, check_password_hash
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
-
-
-# app = Flask(__name__)
-# # PostgresSQL congig
-# app.config[
-#     "SQLALCHEMY_DATABASE_URI"
-# ] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
-#     user=os.getenv("POSTGRES_USER"),
-#     passwd=os.getenv("POSTGRES_PASSWORD"),
-#     host=os.getenv("POSTGRES_HOST"),
-#     port=5432,
-#     table=os.getenv("POSTGRES_DB"),
-# )
-
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
-
-
-
-# class UserModel(db.Model):
-#     __tablename__ = "users"
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(), unique=True, nullable=False)
-#     password = db.Column(db.String(), unique=True, nullable=False)
-
-#     def __init__(self, email, password):
-#         self.email = email
-#         self.password = password
-
-#     def __repr__(self):
-#         return f"<User {self.email}>"
-
-
-# class Destination(db.Model):
-#     __tablename__ = "destinations"
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     userId = db.Column(db.String(), db.ForeignKey('users.id'))
-#     order = db.Column(db.Integer)
-#     address = db.Column(db.String())
-#     daysToStay = db.Column(db.Integer)
-
-#     def __init__(self, userId, order, address, daysToStay):
-#         self.userId = userId
-#         self.order = order
-#         self.address = address
-#         self.daysToStay = daysToStay
-
-
-
-# # home route
-# @app.route("/")
-# def hello_world():
-#     return render_template("index.html")
-
-
-# # destinations route will GET list of destinations, or POST list of destinations from user
-# @app.route("/destinations", methods=("GET", "POST"))
-# def get_destinations():
-#     if request.method == "GET":
-#         # Logic to handle GET request
-#         # return list of destinations by userID
-
-
-#     else:
-#         # Logic to handle POST request
-#         # Add list of destinations
-
-
-#     # Register route logic
-# @app.route("/register", methods=("GET", "POST"))
-# def register():
-#     if request.method == "POST":
-#         email = request.form.get("email")
-#         password = request.form.get("password")
-#         error = None
-
-#         if not email:
-#             error = "email is required."
-#         elif not password:
-#             error = "Password is required."
-#         elif UserModel.query.filter_by(email=email).first() is not None:
-#             error = f"User {email} is already registered."
-
-#         if error is None:
-#             new_user = UserModel(email, generate_password_hash(password))
-#             db.session.add(new_user)
-#             db.session.commit()
-#             return f"User {email} created successfully"
-#         else:
-#             return error, 418
-
-#     ## TODO: Return a restister page
-#     return render_template("register.html")
-
-
-# @app.route("/login", methods=("GET", "POST"))
-# def login():
-#     if request.method == "POST":
-#         email = request.form.get("email")
-#         password = request.form.get("password")
-#         error = None
-#         user = UserModel.query.filter_by(email=email).first()
-
-#         if user is None:
-#             error = "Incorrect email."
-#         elif not check_password_hash(user.password, password):
-#             error = "Incorrect password."
-
-#         if error is None:
-#             return "Login Successful", 200
-#         else:
-#             return error, 418
-
-#     ## TODO: Return a login page
-#     return render_template("login.html")
