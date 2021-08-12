357f25f049402add4b8857a6bb1bc03e9845234f
Brianna Patrick <briannapatrick2020@gmail.com>
2021-08-12 07:59:34 -0400

    plus button and view trips page

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,1 +1,9 @@
-//Main js code here
+/* plus button */
+function addDestination(form) {
+
+    document.getElementById("location").innerHTML += '<i class="material-icons">location_on</i>'
+    + '<label for="adest">Enter Next Location:</label><br>'
+    + '<input type="text" id="adest" name="adest" value=""><br>';
+
+
+  }
