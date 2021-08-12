2eca9ec77b76a6adc3059a47272d75df4d7d88d7
Brianna Patrick <briannapatrick2020@gmail.com>
2021-08-12 11:01:47 -0400

    updated popup css & removed JS for plusbtn

--- app/static/js/main.js
+++
@@ -1,9 +0,0 @@
-/* plus button */
-function addDestination(form) {
-
-    document.getElementById("location").innerHTML += '<i class="material-icons">location_on</i>'
-    + '<label for="adest">Enter Next Location:</label><br>'
-    + '<input type="text" id="adest" name="adest" value=""><br>';
-
-
-  }
