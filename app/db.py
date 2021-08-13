2a881ad97f951fecc70db096804cb2f1f72199c8
Lint Action <lint-action@samuelmeuli.com>
2021-08-13 02:36:19 +0000

    Fix code style issues with Black

--- app/db.py
+++ app/db.py
@@ -1,9 +1,18 @@
 import os
-from flask import Flask, redirect, url_for, session, render_template, send_from_directory, request
+from flask import (
+    Flask,
+    redirect,
+    url_for,
+    session,
+    render_template,
+    send_from_directory,
+    request,
+)
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
@@ -23,16 +32,16 @@ app = Flask(__name__)
 # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-#migrate = Migrate(app, db)
+# migrate = Migrate(app, db)
 
-#User model
+# User model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(), unique=True, nullable=False)
     name = db.Column(db.String(), default=None)
     profile_pic = db.Column(db.String())
-    # 1 -> many relationship with User -> Destination 
-    trip = db.relationship('Trip', backref='user', lazy=True)
+    # 1 -> many relationship with User -> Destination
+    trip = db.relationship("Trip", backref="user", lazy=True)
 
     def __init__(self, email, name, profile_pic):
         self.email = email
@@ -42,12 +51,13 @@ class User(db.Model):
     def __repr__(self):
         return f"User('{self.email}, '{self.name}')"
 
+
 # Trip Model
 class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), unique= True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    destination = db.relationship('Destination', backref="trip", lazy = True)
+    name = db.Column(db.String(), unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    destination = db.relationship("Destination", backref="trip", lazy=True)
 
     def __init__(self, name, user_id):
         self.name = name
@@ -56,6 +66,7 @@ class Trip(db.Model):
     def __repr__(self):
         return f"Trip('{self.name}', '{self.user_id}')"
 
+
 # Destination model
 class Destination(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -63,7 +74,7 @@ class Destination(db.Model):
     alias = db.Column(db.String())
     address = db.Column(db.String())
     daysToStay = db.Column(db.Integer)
-    trip_id = db.Column(db.Integer, db.ForeignKey('trip.id'))
+    trip_id = db.Column(db.Integer, db.ForeignKey("trip.id"))
 
     def __init__(self, order, address, alias, daysToStay, trip_id):
         self.order = order
@@ -72,13 +83,13 @@ class Destination(db.Model):
         self.daysToStay = daysToStay
         self.trip_id = trip_id
 
-    
     def __repr__(self):
         return f"Destinations('{self.order}', '{self.alias}', '{self.address}', '{self.daysToStay}', '{self.trip_id})"
 
+
 # stores user information into db
 def storeInDb(userInfo):
-    email = userInfo['email']
+    email = userInfo["email"]
     name = userInfo["name"]
     # Will only store email if email does not already exist in db
     if Destination.query.filter_by(email=email).first() is not None:
@@ -92,26 +103,26 @@ def storeInDb(userInfo):
             return f"User {email} created successfully"
         else:
             return error, 418
-    else: return
-
+    else:
+        return
 
 
 # updates userDestinations
 # TODO: test this route and make sure it updates based on user_id, add checks
 def updateUserDestination(userId, userInfo):
     updateRow = Destination.query.filter_by(id=userId).first()
-    updateRow.order =userInfo["order"]
+    updateRow.order = userInfo["order"]
     updateRow.address = userInfo["address"]
     updateRow.alias = userInfo["alias"]
-    updateRow.daysToStay =userInfo["daysToStay"]
+    updateRow.daysToStay = userInfo["daysToStay"]
     updateRow.user_id = userId
     db.session.add(updateRow)
     db.session.commit()
 
 
-#deletes userData
+# deletes userData
 def deleteUserInfo(userId):
-    delete = Destination.query.filter_by(id=userId).first() 
+    delete = Destination.query.filter_by(id=userId).first()
     db.session.delete(delete)
 
 
