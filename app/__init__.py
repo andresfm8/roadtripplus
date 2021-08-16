4295b09f618f79970ee5a6661e943b113e26a6f0
Frankie Murillo <frankidatank@gmail.com>
2021-08-16 13:33:56 -0700

    added auth integraion to trips page, name and picture

--- app/__init__.py
+++ app/__init__.py
@@ -51,9 +51,11 @@ google = oauth.register(
 
 @app.route("/")
 def landing_page():
-    # email = dict(session).get("email", None)
-    # name = dict(session).get("name", None)
     return render_template("landing.html")
+
+@app.route("/trips")
+def trips_page():  
+    return render_template("trips.html")
 
 
 @app.route("/planner")
@@ -77,13 +79,13 @@ def authorize():
     resp = google.get("userinfo")  # userinfo contains stuff u specificed in the scrope
     user_info = resp.json()
     # store email into db
-    # storeInDb(user_info)
+    addUser(user_info)
     # print(user_info)
     # stores the user email, name, and picture into session storage
     session["email"] = user_info["email"]
     session["name"] = user_info["name"]
     session["picture"] = user_info["picture"]
-    return redirect("/")
+    return render_template("trips.html", user=user_info)
 
 
 @app.route("/logout")
@@ -91,3 +93,104 @@ def logout():
     for key in list(session.keys()):
         session.pop(key)
     return redirect("/")
+
+#----------------------------------------------------------------
+# db logic and helpers
+# User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(), unique=True, nullable=False)
+    name = db.Column(db.String(), default=None)
+    profile_pic = db.Column(db.String())
+    # 1 -> many relationship with User -> Destination
+    trip = db.relationship("Trip", backref="user", lazy=True)
+
+    def __init__(self, email, name, profile_pic):
+        self.email = email
+        self.name = name
+        self.profile_pic = profile_pic
+
+    def __repr__(self):
+        return f"User('{self.email}, '{self.name}')"
+
+
+# Trip Model
+class Trip(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    destination = db.relationship("Destination", backref="trip", lazy=True)
+
+    def __init__(self, name, user_id):
+        self.name = name
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f"Trip('{self.name}', '{self.user_id}')"
+
+
+# Destination model
+class Destination(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order = db.Column(db.Integer)
+    alias = db.Column(db.String())
+    address = db.Column(db.String())
+    daysToStay = db.Column(db.Integer)
+    trip_id = db.Column(db.Integer, db.ForeignKey("trip.id"))
+
+    def __init__(self, order, address, alias, daysToStay, trip_id):
+        self.order = order
+        self.address = address
+        self.alias = alias
+        self.daysToStay = daysToStay
+        self.trip_id = trip_id
+
+    def __repr__(self):
+        return (
+            f"Destinations("
+            + "'{self.order}', '{self.alias}', '{self.address}', "
+            + "'{self.daysToStay}', '{self.trip_id}')"
+        )
+
+
+# stores user information into db
+def addUser(userInfo):
+    email = userInfo["email"]
+    name = userInfo["name"]
+    # Will only store email if email does not already exist in db
+    if Destination.query.filter_by(email=email).first() is not None:
+        error = None
+        if not email:
+            error = "email is required."
+        if error is None:
+            new_user = User(email=email, name=name)
+            db.session.add(new_user)
+            db.session.commit()
+            print(User.query.first())
+            return f"User {email} created successfully"
+        else:
+            return error, 418
+    else:
+        return
+
+
+# updates userDestinations
+# TODO: test this route and make sure it updates based on user_id, add checks
+def updateUserDestination(userId, userInfo):
+    updateRow = User.query.filter_by(id=userId).first()
+    updateRow.order = userInfo["order"]
+    updateRow.address = userInfo["address"]
+    updateRow.alias = userInfo["alias"]
+    updateRow.daysToStay = userInfo["daysToStay"]
+    updateRow.user_id = userId
+    db.session.add(updateRow)
+    db.session.commit()
+
+
+# deletes userData
+def deleteUserInfo(userId):
+    delete = Destination.query.filter_by(id=userId).first()
+    db.session.delete(delete)
+
+
+# TODO: add more routes that talks to front end to send back User object
