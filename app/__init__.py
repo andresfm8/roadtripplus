11ed71ada4bc7ff3fe458721fb836a130c24fcc7
Frankie Murillo <frankidatank@gmail.com>
2021-08-17 09:46:08 -0700

    added auth integraion to trips page, name and picture

--- app/__init__.py
+++ app/__init__.py
@@ -48,77 +48,29 @@ google = oauth.register(
     client_kwargs={"scope": "openid email profile"},
 )
 
-
-@app.route("/")
-def landing_page():
-    return render_template("landing.html")
-
-@app.route("/trips")
-def trips_page():  
-    return render_template("trips.html")
-
-
-@app.route("/planner")
-def planner_page():
-    return render_template("planner.html")
-
-
-@app.route("/login")
-def login():
-    google = oauth.create_client("google")  # create the google oauth client
-    redirect_uri = url_for("authorize", _external=True)
-    return google.authorize_redirect(redirect_uri)
-
-
-@app.route("/authorize")
-def authorize():
-    google = oauth.create_client("google")  # create the google oauth client
-    # token = (
-    #    google.authorize_access_token()
-    # )  # Access token from google (needed to get user info)
-    resp = google.get("userinfo")  # userinfo contains stuff u specificed in the scrope
-    user_info = resp.json()
-    # store email into db
-    addUser(user_info)
-    # print(user_info)
-    # stores the user email, name, and picture into session storage
-    session["email"] = user_info["email"]
-    session["name"] = user_info["name"]
-    session["picture"] = user_info["picture"]
-    return render_template("trips.html", user=user_info)
-
-
-@app.route("/logout")
-def logout():
-    for key in list(session.keys()):
-        session.pop(key)
-    return redirect("/")
-
 #----------------------------------------------------------------
 # db logic and helpers
 # User model
-class User(db.Model):
+class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(), unique=True, nullable=False)
-    name = db.Column(db.String(), default=None)
-    profile_pic = db.Column(db.String())
+    name = db.Column(db.String(20), default=None)
     # 1 -> many relationship with User -> Destination
-    trip = db.relationship("Trip", backref="user", lazy=True)
+    trips = db.relationship("Trip", backref="person", lazy=True)
 
-    def __init__(self, email, name, profile_pic):
+    def __init__(self, email, name):
         self.email = email
         self.name = name
-        self.profile_pic = profile_pic
 
     def __repr__(self):
-        return f"User('{self.email}, '{self.name}')"
+        return f"Person('{self.email}, '{self.name}')"
 
 
 # Trip Model
 class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), unique=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    person_id = db.Column(db.Integer, db.ForeignKey("person.id"))
     destination = db.relationship("Destination", backref="trip", lazy=True)
 
     def __init__(self, name, user_id):
@@ -158,18 +110,17 @@ def addUser(userInfo):
     email = userInfo["email"]
     name = userInfo["name"]
     # Will only store email if email does not already exist in db
-    if Destination.query.filter_by(email=email).first() is not None:
+    if Person.query.filter_by(email=email).first() is not None:
         error = None
         if not email:
             error = "email is required."
         if error is None:
-            new_user = User(email=email, name=name)
+            new_user = Person(email, name)
             db.session.add(new_user)
             db.session.commit()
-            print(User.query.first())
-            return f"User {email} created successfully"
+            return f"User {email}, {name} created successfully"
         else:
-            return error, 418
+            return error, 418 
     else:
         return
 
@@ -177,7 +128,7 @@ def addUser(userInfo):
 # updates userDestinations
 # TODO: test this route and make sure it updates based on user_id, add checks
 def updateUserDestination(userId, userInfo):
-    updateRow = User.query.filter_by(id=userId).first()
+    updateRow = Person.query.filter_by(id=userId).first()
     updateRow.order = userInfo["order"]
     updateRow.address = userInfo["address"]
     updateRow.alias = userInfo["alias"]
@@ -194,3 +145,60 @@ def deleteUserInfo(userId):
 
 
 # TODO: add more routes that talks to front end to send back User object
+
+
+
+@app.route("/")
+def landing_page():
+    return render_template("landing.html")
+
+@app.route("/trips")
+def trips_page():  
+    return render_template("trips.html")
+
+
+@app.route("/planner")
+def planner_page():
+    return render_template("planner.html")
+
+
+@app.route("/login")
+def login():
+    google = oauth.create_client("google")  # create the google oauth client
+    redirect_uri = url_for("authorize", _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+
+@app.route("/authorize")
+def authorize():
+    google = oauth.create_client("google")  # create the google oauth client
+    token = (
+        google.authorize_access_token()
+     )  # Access token from google (needed to get user info)
+    resp = google.get("userinfo")  # userinfo contains stuff u specificed in the scrope
+    user_info = resp.json()
+    # store email into db
+    addUser(user_info)
+    # print(user_info)
+    # stores the user email, name, and picture into session storage
+    session["email"] = user_info["email"]
+    session["name"] = user_info["name"]
+    session["picture"] = user_info["picture"]
+    return render_template("trips.html", user=user_info)
+
+
+@app.route("/logout")
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect("/")
+
+@app.before_first_request
+def before_req_func():
+    db.drop_all()
+    db.create_all()
+
+
+if __name__ == "__main__":
+    db.init_app(app)
+    app.run(host="0.0.0.0")
